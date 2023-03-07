d4919454627018adc8f06236f3a43e524b24a814
DevEd <deved8@gmail.com>
2023-03-07 13:59:44 -0500

    fix regression in pitch delay system

--- Audio/DevSoundX.asm
+++ Audio/DevSoundX.asm
@@ -897,9 +897,6 @@ DevSoundX_UpdateChannel\1:
     ld      [DSX_CH\1_VolDelay],a
     ld      [DSX_CH\1_ArpDelay],a
     ld      [DSX_CH\1_PulseDelay],a
-    if \1 != 4
-        ld      [DSX_CH\1_PitchDelay],a
-    endc
     jp      .donech\1
 .isrest
     ld      [DSX_CH\1_Note],a
